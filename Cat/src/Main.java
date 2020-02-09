30c773f03e6e63a873a443d55469a8e75f918613
19952104 <ana_bologa95@yahoo.com>
2020-02-09 18:23:58 +0200

    Initial commit

---
+++ Cat/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        cat piki = new cat();
+        piki.name = "Piki";
+        piki.favoriteActivity = "sleep";
+        piki.favoriteFood = " felix";
+        piki.healthStatus = 7;
+        piki.levelOfSpirit = 8;
+        piki.theLevelOfTheFeelingOfHunger = 10;
+        System.out.println(piki.name + "\n " + piki.theLevelOfTheFeelingOfHunger + "\n" + piki.levelOfSpirit + "\n " + piki.healthStatus + "\n " + piki.favoriteFood + "\n" + piki.favoriteActivity);
+
+
+    }
+}
---
+++ Cat/src/cat.java
@@ -0,0 +1,11 @@
+public class cat {
+    String name;
+    float age;
+    int healthStatus;
+    int theLevelOfTheFeelingOfHunger;
+    int levelOfSpirit;
+    String favoriteFood;
+    String favoriteActivity;
+
+
+}
